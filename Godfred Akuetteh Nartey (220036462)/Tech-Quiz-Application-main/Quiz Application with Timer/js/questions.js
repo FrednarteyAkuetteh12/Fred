e28aee91c79d64ab4a0681b196c1e9f51e603971
FrednarteyAkuetteh12 <130565672+FrednarteyAkuetteh12@users.noreply.github.com>
2023-04-12 12:30:39 +0000

    Add files via upload

---
+++ Godfred Akuetteh Nartey (220036462)/Tech-Quiz-Application-main/Quiz Application with Timer/js/questions.js
@@ -0,0 +1,116 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is not a listening model?",
+    answer: "Distraction model",
+    options: [
+      "Action model",
+      "Distraction model",
+      "Transaction model",
+      "Interaction model"
+    ]
+  },
+    {
+    numb: 2,
+    question: "__________ includes all the accounting expenses as well as the opportunity cost of a business firm?",
+    answer: "Economic cost",
+    options: [
+      "Private cost",
+      "External cost",
+      "Accounting cost",
+      "Economic cost"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The following are types of ocean fishing method except?",
+    answer: "Basket and trap method",
+    options: [
+      "Drifting method",
+      "Beach seine method",
+      "Trawling method",
+      "Basket and trap method"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is an inland fishing method?",
+    answer: "Basket and trap method",
+    options: [
+      "Drifting method",
+      "Basket and trap method",
+      "Trawling method",
+      "Beach seine method",
+      
+    ]
+  },
+    {
+    numb: 5,
+    question: "___________ is formed when enormous magma cools down slowly to form large crystal rock?",
+    answer: "Batholith",
+    options: [
+      "Batholith",
+      "Phacolith",
+      "Lopolith",
+      "Lava"
+    ]
+  },
+  
+    {
+    numb: 6,
+    question: "Javascript is an _________ language?",
+    answer: "Object-Oriented",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    answer: "Both A and B",
+    options: [
+      "var",
+      "let",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which country won the 2022 fifa world cup?",
+    answer: "Argentina",
+    options: [
+      "France",
+      "Brazil",
+      "Argentina",
+      "Portugal"
+    ]
+  
+  },
+  {
+    numb: 9,
+    question: "Which of the following is not a Javascript framework?",
+    answer: "Cassandra",
+    options: [
+      "Node",
+      "Vue",
+      "React",
+      "Cassandra"
+    ]
+  },
+  {
+    numb: 10,
+    question: "In the history of football, which team has won the champions league thrice in a row?",
+    answer: "Real madrid fc",
+    options: [
+      "Barcelona fc",
+      "Bayern munchen fc",
+      "Juventus fc ",
+      "Real madrid fc"
+    ]
+  }
+];
